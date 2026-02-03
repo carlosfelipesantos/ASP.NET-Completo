e84e29476f9f5ec3cb4b7697132a9d93cbc90e99
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-02-03 16:48:19 -0300

    Configurando Projeto para EF

---
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; } //Entity Framework entende que Id= chave primaria, e nome da classe+id tambem (Categoria+Id)
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+
+    }
+}
