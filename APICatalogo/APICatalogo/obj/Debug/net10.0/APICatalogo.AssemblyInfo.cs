51d0dc81031def142b1aac5e366270a6df78ecc3
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-02-04 16:42:22 -0300

    Populando Tabela Produtos

--- APICatalogo/APICatalogo/obj/Debug/net10.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net10.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da69dd09348a92969ce46281bb01e89a5ac5767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e92b0d5b387e3963db4c8ee23b66aa9747a1725c")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
