49c5579d46b56328a03a83e7f3df17591723d191
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-02-05 17:01:22 -0300

    Action Methods Put

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -47,5 +47,16 @@ namespace APICatalogo.Controllers
             return CreatedAtAction(nameof(Get), new { id = produto.ProdutoId }, produto);
         }
 
+        [HttpPut("{id:int}")]
+        public ActionResult<Produto> Put(int id, Produto produto)
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest("Id do produto não corresponde ao id da URL");
+            }
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(produto);
+        } 
     }
 }
